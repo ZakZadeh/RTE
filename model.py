5db39500d5d783b7487999803224b62e2bad7f36
ZakZadeh <zak.zadeh@icloud.com>
2022-09-13 17:10:26 -0500

    1.0

--- model.py
+++ model.py
@@ -103,7 +103,7 @@ class CatFusion(nn.Module):
     def forward(self, x1, x2):
         x = torch.cat((x1, x2), 1)
         x = self.fc1(x)
-        x = self.fc3(x)
+        x = self.fc2(x)
         return x
 
 """ -------------------------------------------------------------------------"""
